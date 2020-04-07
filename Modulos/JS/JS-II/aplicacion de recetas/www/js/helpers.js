a4870ea601499237862492563a7c910176f98769
Rubén Pérez <rubenpo167@gmail.com>
2020-04-07 20:31:18 +0200

    aplicacion de recetas

---
+++ Modulos/JS/JS-II/aplicacion de recetas/www/js/helpers.js
@@ -0,0 +1,15 @@
+async function getData(url) {
+  const response = await fetch(url);
+  const json = await response.json();
+  return json;
+}
+
+function getRecipeImage(recipe) {
+  const image = document.createElement("img");
+  image.setAttribute("src", recipe.strMealThumb);
+  image.setAttribute("alt", recipe.strMeal);
+
+  return image;
+}
+
+export { getData, getRecipeImage };
