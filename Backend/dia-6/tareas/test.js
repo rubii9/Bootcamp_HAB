5a3111d63799f6ff585139962c5b5d7cdc107b10
Rubén Pérez <rubenpo167@gmail.com>
2020-04-25 19:04:38 +0200

    ejercicio entrega 8

--- Backend/dia-6/tareas/test.js
+++ Backend/dia-6/tareas/test.js
@@ -3,6 +3,7 @@ const path = require("path");
 const minimist = require("minimist");
 const dateFns = require("date-fns");
 const { format } = require("date-fns");
+const chalk = require("chalk");
 
 const todoPath = "./.tasks.json";
 const { _, priority, done, undone, clean, list } = minimist(
@@ -25,6 +26,7 @@ async function comprobarLista() {
   }
 }
 
+//Crear el mensaje
 async function buildMessage() {
   let text = _.join(" ");
   let added = new Date();
@@ -34,31 +36,73 @@ async function buildMessage() {
   await buildArray(message);
 }
 
+//Añadir mensaje al array de tareas
 async function buildArray(message) {
   let listaTareas = await comprobarLista();
   listaTareas.tasks.push(message);
-  console.log(listaTareas);
   fs.writeFile(todoPath, JSON.stringify(listaTareas));
+  for (const tarea of listaTareas.tasks) {
+    console.log(chalk`
+    {blue.bold Nombre:} ${chalk.yellow.bold(tarea.text)}
+    {blue.bold Prioridad:} {magenta ${
+      tarea.priority === undefined
+        ? chalk.greenBright("normal")
+        : chalk.bold.redBright.underline("alta")
+    }}
+    {blue.bold Estado:}  ${
+      tarea.done === true
+        ? chalk.greenBright("completada")
+        : chalk.bold.magentaBright("pendiente")
+    }
+    {blue.bold Añadida:} {magenta ${tarea.added}}
+    `);
+  }
 }
 
+//Mostrar las tareas
 async function showList() {
   let listaTareas = await comprobarLista();
 
   for (const tarea of listaTareas.tasks) {
-    console.log(`Tarea: ${tarea.text}`);
-    console.log(`Prioridad: ${tarea.priority}`);
-    console.log(`Echa: ${tarea.done}`);
-    console.log(`Añadida: ${tarea.added}`);
-    console.log("\n");
+    console.log(chalk`
+    {blue.bold Nombre:} ${chalk.yellow.bold(tarea.text)}
+    {blue.bold Prioridad:} {magenta ${
+      tarea.priority === undefined
+        ? chalk.greenBright("normal")
+        : chalk.bold.redBright.underline("alta")
+    }}
+    {blue.bold Estado:}  ${
+      tarea.done === true
+        ? chalk.greenBright("completada")
+        : chalk.bold.magentaBright("pendiente")
+    }
+    {blue.bold Añadida:} {magenta ${tarea.added}}
+    `);
   }
 }
+
+//Marcar como hechas las tareas
 async function makeDone(index, value) {
   let listaActualizada = await comprobarLista();
   listaActualizada.tasks[index].done = value;
   fs.writeFile(todoPath, JSON.stringify(listaActualizada));
-  console.log(listaActualizada.tasks[index]);
+  console.log(chalk`
+    {blue.bold Nombre:} ${chalk.yellow.bold(listaActualizada.tasks[index].text)}
+    {blue.bold Prioridad:} {magenta ${
+      listaActualizada.tasks[index].priority === undefined
+        ? chalk.greenBright("normal")
+        : chalk.bold.redBright.underline("alta")
+    }}
+    {blue.bold Estado:}  ${
+      listaActualizada.tasks[index].done === true
+        ? chalk.greenBright("completada")
+        : chalk.bold.magentaBright("pendiente")
+    }
+    {blue.bold Añadida:} {magenta ${listaActualizada.tasks[index].added}}
+    `);
 }
 
+//Borrar tareas completadas
 async function cleanDoneList() {
   let listaActualizada = await comprobarLista();
   for (let i = 0; i < listaActualizada.tasks.length; i++) {
@@ -67,14 +111,14 @@ async function cleanDoneList() {
     }
   }
   fs.writeFile(todoPath, JSON.stringify(listaActualizada));
-  console.log("Borradas las tareas hechas!");
+  console.log("Borradas las tareas completadas!");
 }
 
 if (list) {
   showList();
 } else if (done || done === 0) {
   makeDone(done, true);
-} else if (undone || done === 0) {
+} else if (undone || undone === 0) {
   makeDone(undone, false);
 } else if (clean) {
   cleanDoneList();
