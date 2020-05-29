bef699b18e45805df9028a82b3521e27608c8f5a
rubii9 <rubenpo167@gmail.com>
2020-05-29 22:36:58 +0200

    ejercicio rickymorty

--- Front-Vuejs/rickmortyapi/src/App.vue
+++ Front-Vuejs/rickmortyapi/src/App.vue
@@ -1,8 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About me</router-link>
+      <router-link to="/">Home</router-link>
     </div>
     <router-view />
   </div>
@@ -15,10 +14,15 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+  background-color: #ffcc99;
 }
 
 #nav {
   padding: 30px;
+}
+
+body {
+  margin: 0;
 }
 
 #nav a {
